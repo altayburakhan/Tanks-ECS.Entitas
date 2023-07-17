ac452c1d74d05ab42ecb78964ca7f288cb231057
Burakhan Altay <burakhan.altay@gmail.com>
2023-07-18 00:25:43 +0300

    Jenny-properties fixed

--- Assets/HealthComponent.cs
+++ Assets/HealthComponent.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Entitas;
 
-public class HealthComponent : MonoBehaviour
+public class HealthComponent : IComponent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public float value;
 }
